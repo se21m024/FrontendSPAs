0191c69b1715ecf9cab161a0becf2d41761259b2
Thomas Stummer <thomas.stummer@grassfish.com>
2022-03-31 12:36:36 +0200

    Auto complete countries

--- AutoCompleteWebComponent/AutoCompleteTextInputLit.js
+++ AutoCompleteWebComponent/AutoCompleteTextInputLit.js
@@ -1,16 +1,22 @@
-//import { LitElement, html } from 'lit-element';
 import { LitElement, html } from 'lit-element/lit-element.js';
-
-//import { LitElement, html } from './node_modules/lit-element/lit-element.js';
-//import { html, LitElement } from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
 
 class AutoCompleteTextInput extends LitElement {
   
     render() {
     return html`
         <div>
-            <label for="auto-complete-text-input">${this.title}</label><br>
-            <input type="text" id="auto-complete-text-input" name="auto-complete-text-input">
+            <label>${this.title}</label><br>
+            <input type="text" 
+                id="auto-complete-text-input"
+                name="auto-complete-text-input" 
+                placeholder="Type your country here..."
+                @keyup=${e => this.onInputTextChanged(e.target.value)}>
+            <br/>
+            <input type="hidden"
+                id="auto-complete-suggestion"
+                name="auto-complete-suggestion"
+                @click=${e => this.onSuggestionClick(e.target.value)}
+                readonly>
         </div>
     `;
     }
@@ -18,15 +24,59 @@ class AutoCompleteTextInput extends LitElement {
     constructor() {
         super();
         this.title = 'Default title';
+        this.addEventListener('focusout', this.onFocusout);
     }
 
-    static get properties(){
+    static get properties() {
         return{
             title: { type: String },
+            suggestions: { type: Array }
         };
     }
 
+    onFocusout(e) {
+        console.log("Auto complete text input lost focus.")
+    }
+
+    onInputTextChanged(text) {
+        console.log("Text changed to: " + text)
+        
+        // Hide suggestion box
+        if(text.length < 1) {
+            this.shadowRoot
+                .getElementById("auto-complete-suggestion")
+                .setAttribute("type", "hidden")
+            return
+        }
+
+        // Search for match
+        let suggestion = this.suggestions.find(x => x.toLowerCase().startsWith(text.toLowerCase()))
+        console.log("New suggestion: " + suggestion)
+
+        // Hide suggestion box if no match found
+        if(suggestion == undefined || suggestion.length < 1) {
+            this.shadowRoot
+                .getElementById("auto-complete-suggestion")
+                .setAttribute("type", "hidden")
+            return
+        }
+
+        // Show suggestion box if match found
+        this.shadowRoot
+            .getElementById("auto-complete-suggestion")
+            .setAttribute("placeholder", suggestion)
+        this.shadowRoot
+            .getElementById("auto-complete-suggestion")
+            .setAttribute("type", "text")
+    }
+
+    onSuggestionClick(suggestion) {
+        console.log("Suggestion box was clicked.")
+        this.shadowRoot
+            .getElementById("auto-complete-text-input")
+            .setAttribute("value", suggestion)
+    }
 }
 
-// Name the component
+// Component name
 customElements.define('my-auto-complete-text-input', AutoCompleteTextInput); 
